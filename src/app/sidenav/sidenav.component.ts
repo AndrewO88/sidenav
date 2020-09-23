b778a48103ec1aaa6b438a8ea79508fab2e1d3a4
ando8841 <oleshchuk91@bk.ru>
2020-09-23 19:05:34 +0300

    +ViewChild

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+  @ViewChild('sidenav') public sidenav: MatSidenav;
   image = 'https://memepedia.ru/wp-content/uploads/2016/03/large_p19d7nh1hm1i37tnuim11ebqo5c1.jpg';
   constructor() {
   }
@@ -13,7 +15,9 @@ export class SidenavComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  click(): void {
-    console.log('хуй');
+  onClick(): void {
+    this.sidenav.toggle().then(() => {
+      console.log('хуй');
+    });
   }
 }
